4344c885ff332bcf93cff5e97d65dba56d3ad91e
Maria Jenkins <macjenkin@gmail.com>
2023-10-13 21:31:08 -0500

    create submenu structure

--- lib/cli.py
+++ lib/cli.py
@@ -7,13 +7,25 @@ from helpers import (
 
 
 def main():
-    while True:
+    m = "main"
+    while m == "main":
         menu()
         choice = input("> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            m = "sub"
+            while m == "sub":
+                submenu()
+                choice = input("> ")
+                if choice == "0":
+                    m = "main"
+                elif choice == "1":
+                    helper_1()
+                elif choice == "2":
+                    helper_1()
+                else:
+                    print("Invalid choice")
         else:
             print("Invalid choice")
 
@@ -21,7 +33,13 @@ def main():
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. Submenu")
+
+def submenu():
+    print("Please select an option:")
+    print("0. Go back to main menu")
+    print("1. Option 1")
+    print("2. Option 2")
 
 
 if __name__ == "__main__":
