ad9019d2fa387b6242bacdb0ad81cd1dd952f726
Maria Jenkins <macjenkin@gmail.com>
2023-10-14 03:09:21 -0500

    create seed data

--- lib/seed.py
+++ lib/seed.py
@@ -6,21 +6,10 @@ from models.artist import Artist
 from models.painting import Painting
 from models.movement import Movement
 
-def seed_database():
-    Artist.drop_table()
-    Painting.drop_table()
-    Movement.drop_table()
-    Artist.create_table()
-    Painting.create_table()
-    Movement.create_table()
-
-seed_database()
-print("Seeded database")
-
 movements = ["'85 New Wave", 'Abstract Expressionism', 'American Impressionism', 
              'Ancient Egyptian', 'Arab Modernism', 'Art Deco', 'Art Noveau', 
              'Arte Povera', 'Arts and Crafts', 'Asuka', 'Azuchi-Momoyama', 
-             'Baroda Group of Artists', 'Baroque', 'Bauhaus', 'Bengal School', 
+             'Baroda Group of Artists', 'Bauhaus', 'Bengal School', 
              'Bombay Progressives', 'Byzantine', 'Calcutta Group', 
              'Colonial Era', 'Constructivism', 'Contemporary Art', 
              'Cretan School', 'Cubism', 'De Stijl', 'Deconstructivism', 
@@ -40,8 +29,37 @@ movements = ["'85 New Wave", 'Abstract Expressionism', 'American Impressionism',
              'Rococo', 'Romanesque', 'Romanticism', 'Russian Futurism', 
              'Russian avant-garde', 'Saqqakhaneh', 'Shang Dynasty', 
              'Song Dynasty', 'Southern Dynasty', 'Stone Age', 'Sui Dynasty', 
-             'SuprematismDadaism', 'Surrealism', 'Symbolism', 'Tang Dynasty', 
+             'SuprematismDadaism', 'Symbolism', 'Tang Dynasty', 
              'Three Kingdoms', 'Tonalism', 'Xia Dynasty', 'Yuan Dynasty', 
              'Zhou Dynasty', 'Socialist Realism']
 
- 
+def seed_database():
+    Artist.drop_table()
+    Painting.drop_table()
+    Movement.drop_table()
+    Artist.create_table()
+    Painting.create_table()
+    Movement.create_table()
+
+    academicism = Movement.create("Academicism")
+    baroque = Movement.create("Baroque")
+    neo_expressionism = Movement.create("Neo-Expressionism")
+    surrealism = Movement.create("Surrealism")
+
+    for movement in movements:
+        Movement.create(movement)
+
+    basquiat = Artist.create("Jean-Michel Basquiat", neo_expressionism.id)
+    cabanel = Artist.create("Alexandre Cabanel", academicism.id)
+    dali = Artist.create("Salvador Dali", surrealism.id)
+    gentileschi = Artist.create("Artemisia Gentileschi", baroque.id)
+
+    Painting.create("Judith Slaying Holofernes", "1620", "oil", gentileschi.id)
+    Painting.create("The Elephants", "1948", "oil", dali.id)
+    Painting.create("The Fallen Angel", "1847", "oil", cabanel.id)
+    Painting.create("The Temptation of St. Anthony", "1946", "oil", dali.id)
+    Painting.create("Untitled (Skull)", "1981", "acrylic", basquiat.id)
+
+
+seed_database()
+print("Seeded database")
