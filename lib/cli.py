55856d0b34ea138e411da9ab379a41b9d34f7a9c
Maria Jenkins <macjenkin@gmail.com>
2023-10-13 21:35:41 -0500

    expand menu

--- lib/cli.py
+++ lib/cli.py
@@ -14,9 +14,22 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            m = "sub"
-            while m == "sub":
-                submenu()
+            m = "artists"
+            while m == "artists":
+                artists()
+                choice = input("> ")
+                if choice == "0":
+                    m = "main"
+                elif choice == "1":
+                    helper_1()
+                elif choice == "2":
+                    helper_1()
+                else:
+                    print("Invalid choice")
+        elif choice == "2":
+            m = "paintings"
+            while m == "paintings":
+                paintings()
                 choice = input("> ")
                 if choice == "0":
                     m = "main"
@@ -31,15 +44,25 @@ def main():
 
 
 def menu():
+    print("Main Menu")
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Submenu")
+    print("1. Artists")
+    print("2. Paintings")
 
-def submenu():
+def artists():
+    print("Artist Menu")
     print("Please select an option:")
     print("0. Go back to main menu")
-    print("1. Option 1")
-    print("2. Option 2")
+    print("1. Artist Option 1")
+    print("2. Artist Option 2")
+
+def paintings():
+    print("Paintings Menu")
+    print("Please select an option:")
+    print("0. Go back to main menu")
+    print("1. Painting Option 1")
+    print("2. Painting Option 2")
 
 
 if __name__ == "__main__":
